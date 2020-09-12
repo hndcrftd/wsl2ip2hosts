af53c8bf36cca91cd25d44338a0aa59d05a27796
Jakob Wildrain <jrwildrain@msn.com>
2020-09-11 19:57:40 -0500

    moved script location to users home folder

--- ips2hosts.sh
+++ ips2hosts.sh
@@ -32,7 +32,7 @@ WSLIP=$(ip -4 a show eth0 | grep -Po "inet \K[0-9.]*")
 
 # check if our current IP is already in the windows hosts file (we can read it but not write to it)
 # wslpath command converts from Windows path to a WSL path. We want this in case the drive letter for boot drive is not C:
-windir=$(wslpath $(cmd.exe /c echo %WINDIR% 2>NUL))
+windir=$(wslpath $(cmd.exe /c echo %WINDIR%))
 winhostsfile=${windir%$'\r'}/System32/drivers/etc/hosts
 grep $WSLIP $winhostsfile > /dev/null 2>&1
 # if it's in there grep returns 0, if not it's 1
@@ -41,7 +41,7 @@ then
 	HOMEFOLDER=$(wslpath -w ~)
 	# if we have the new PowerShellCore - use it, it's faster, if not - fallback to the old one
 	type pwsh.exe > /dev/null 2>&1 && PS="pwsh" || PS="powershell"
-	$PS.exe -Command 'start-process -verb runas '$PS' -ArgumentList "-Command &{'$HOMEFOLDER'\wsl2ip2winhosts.ps1 '$WSLIP'}"'
+	$PS.exe -Command 'start-process -verb runas '$PS' -ArgumentList "-Command &{~\wsl2ip2winhosts.ps1 '$WSLIP'}"'
 fi
 
 # set this to 1 to start httpd on WSL start or 0 to skip
